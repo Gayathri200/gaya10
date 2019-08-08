67fe53306e9303ca26733fd2f7b5fa6a0f8d2cb6
gayathri.m <53171194+Gayathri200@users.noreply.github.com>
2019-08-09 03:19:40 +0530

    Create line1.py

---
+++ line1.py
@@ -0,0 +1,15 @@
+x1,y1=map(int,input().split())
+x2,y2=map(int,input().split())
+x3,y3=map(int,input().split())
+if(x1==x2==x3):
+  print("yes")
+elif(y1==y2==y3):
+  print("yes")
+elif(x1==y1):
+  print("yes")  
+elif(x2==y2):
+  print("yes") 
+elif(x3==y3):
+  print("yes")     
+else:
+  print("no")      
